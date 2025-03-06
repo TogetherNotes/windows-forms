19f26d01ced19cb1d76701b18d2e0b2ae58ea368
Juan <jabritomar@gmail.com>
2025-03-06 20:18:18 +0100

    fix code

--- TogetherNotes/Forms/MainWindow.xaml.cs
+++ TogetherNotes/Forms/MainWindow.xaml.cs
@@ -13,19 +13,6 @@ namespace TogetherNotes.Forms
             InitializeComponent();
         }
 
-        private void CloseWindow(object sender, RoutedEventArgs e)
-        {
-            ExitConfirmation exitPopup = new ExitConfirmation();
-            exitPopup.Owner = this; // Assegura que la finestra modal es centri sobre la principal
-
-            bool? result = exitPopup.ShowDialog(); // Mostra la finestra i espera resposta
-
-            if (result == true)
-            {
-                Application.Current.Shutdown();
-            }
-        }
-
         private void MinimizeWindow(object sender, RoutedEventArgs e)
         {
             this.WindowState = WindowState.Minimized;
