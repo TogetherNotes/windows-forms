fddc04c07fd8c20b34abd1eb3f0d682462ed3cd3
rwxce <148581386+rwxce@users.noreply.github.com>
2025-03-12 16:38:30 +0100

    chg and add faqs translations

--- TogetherNotes/Forms/Settings.xaml.cs
+++ TogetherNotes/Forms/Settings.xaml.cs
@@ -1,28 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TogetherNotes.Forms
 {
-    /// <summary>
-    /// Lógica de interacción para Settings.xaml
-    /// </summary>
     public partial class Settings : UserControl
     {
         public Settings()
         {
             InitializeComponent();
+        }
+
+        private void SetLanguage(string languageCode)
+        {
+            CultureInfo culture = new CultureInfo(languageCode);
+            Thread.CurrentThread.CurrentUICulture = culture;
+            Thread.CurrentThread.CurrentCulture = culture;
+
+            ResourceDictionary dict = new ResourceDictionary
+            {
+                Source = new Uri($"/Languages/strings.{languageCode}.xaml", UriKind.Relative)
+            };
+
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(dict);
+        }
+
+        private void SetCatalan(object sender, RoutedEventArgs e)
+        {
+            SetLanguage("ca");
+        }
+
+        private void SetSpanish(object sender, RoutedEventArgs e)
+        {
+            SetLanguage("es");
+        }
+
+        private void SetEnglish(object sender, RoutedEventArgs e)
+        {
+            SetLanguage("en");
         }
     }
 }
--- TogetherNotes/ViewModel/ShipmentVM.cs
+++ TogetherNotes/ViewModel/ShipmentVM.cs
@@ -1,33 +1,25 @@
 ﻿using System.Collections.ObjectModel;
-using TogetherNotes.Models;
 using TogetherNotes.Utils;
 
 namespace TogetherNotes.ViewModel
 {
     class ShipmentVM : ViewModelBase
     {
-        private readonly PageModel _pageModel;
-
-        private ObservableCollection<ShipmentFAQ> _questions;
-        public ObservableCollection<ShipmentFAQ> Questions
-        {
-            get { return _questions; }
-            set
-            {
-                _questions = value;
-                OnPropertyChanged(nameof(Questions));
-            }
-        }
+        public ObservableCollection<ShipmentFAQ> Questions { get; set; }
 
         public ShipmentVM()
         {
-            _pageModel = new PageModel();
             Questions = new ObservableCollection<ShipmentFAQ>
-                {
-                    new ShipmentFAQ { Question = "How can I track my shipment?", Answer = "Use your tracking ID in the shipment portal." },
-                    new ShipmentFAQ { Question = "What should I do if my package is delayed?", Answer = "Check the tracking details or contact support." },
-                    new ShipmentFAQ { Question = "Can I change my delivery address?", Answer = "Yes, within 24 hours of placing the order." }
-                };
+            {
+                new ShipmentFAQ { Question = (string)App.Current.Resources["Faq1Question"],
+                                  Answer = (string)App.Current.Resources["Faq1Answer"] },
+
+                new ShipmentFAQ { Question = (string)App.Current.Resources["Faq2Question"],
+                                  Answer = (string)App.Current.Resources["Faq2Answer"] },
+
+                new ShipmentFAQ { Question = (string)App.Current.Resources["Faq3Question"],
+                                  Answer = (string)App.Current.Resources["Faq3Answer"] }
+            };
         }
     }
 
