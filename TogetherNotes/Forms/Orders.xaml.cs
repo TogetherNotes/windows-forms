f6dc0971eb13ccdbfab15713ae6ecd61505c7387
rwxce <148581386+rwxce@users.noreply.github.com>
2025-03-14 16:46:27 +0100

    map perfect

--- TogetherNotes/Forms/Orders.xaml.cs
+++ TogetherNotes/Forms/Orders.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media;
 using GMap.NET;
 using GMap.NET.MapProviders;
 
@@ -6,10 +8,16 @@ namespace TogetherNotes.Forms
 {
     public partial class Orders : UserControl
     {
+        private RectangleGeometry clipGeometry;
+
         public Orders()
         {
             InitializeComponent();
             LoadMap();
+
+            // Inicialitzem el Clip
+            clipGeometry = new RectangleGeometry();
+            MapControl.Clip = clipGeometry;
         }
 
         private void LoadMap()
@@ -26,7 +34,7 @@ namespace TogetherNotes.Forms
             // Nivell de zoom
             MapControl.MinZoom = 5;
             MapControl.MaxZoom = 18;
-            MapControl.Zoom = 12;
+            MapControl.Zoom = 14;
 
             // Controls d'interacció
             MapControl.CanDragMap = true;
@@ -34,6 +42,25 @@ namespace TogetherNotes.Forms
 
             // Desactiva les etiquetes per un look més net
             MapControl.ShowTileGridLines = false;
+        }
+
+        private void MapControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Establim el Clip inicial
+            UpdateMapClip();
+        }
+
+        private void MapControl_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // Actualitzem el Clip quan el mapa canviï de mida
+            UpdateMapClip();
+        }
+
+        private void UpdateMapClip()
+        {
+            clipGeometry.Rect = new Rect(0, 0, MapControl.ActualWidth, MapControl.ActualHeight);
+            clipGeometry.RadiusX = 20;
+            clipGeometry.RadiusY = 20;
         }
     }
 }
