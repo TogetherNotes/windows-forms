52b2b39487c54a2b621817af93ada3710d6a94e5
Juan <jabritomar@gmail.com>
2025-03-21 17:34:17 +0100

    fix exception method

--- TogetherNotes/Models/Management/Orm.cs
+++ TogetherNotes/Models/Management/Orm.cs
@@ -7,9 +7,33 @@ namespace TogetherNotes.Models
     {
         public static tgtnotesEntities db = new tgtnotesEntities();
 
-        internal static bool ErrorMessage(SqlException ex)
+        public static string ErrorMessage(SqlException sqlException)
         {
-            throw new NotImplementedException();
+            string message = "";
+
+            switch (sqlException.Number)
+            {
+                case 2:
+                    message = "The server is not operational.";
+                    break;
+                case 547:
+                    message = "The record cannot be deleted because it has related records.";
+                    break;
+                case 4060:
+                    message = "Could not connect to the database.";
+                    break;
+                case 18456:
+                    message = "Login failed.";
+                    break;
+                case 2601:
+                    message = "A record with the same value already exists.";
+                    break;
+                default:
+                    message = sqlException.Number + " - " + sqlException.Message;
+                    break;
+            }
+
+            return message;
         }
     }
 }
