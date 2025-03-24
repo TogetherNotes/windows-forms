a69e5c93476cec8281e8db1f66113aa5addaca7a
Juan <jabritomar@gmail.com>
2025-03-24 16:30:28 +0100

    create event class

---
+++ TogetherNotes/Utils/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TogetherNotes.Utils
+{
+    class Event
+    {
+        public Event(long time, string title)
+        {
+            this.Timestamp = time;
+            this.Title = title;
+        }
+
+        public long Timestamp { get; set; }
+        public string Title { get; set; }
+    }
+}
