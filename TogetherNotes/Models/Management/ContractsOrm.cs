498ce800eef17cf24e1992b271dc8bb528b12215
Juan <jabritomar@gmail.com>
2025-03-24 17:10:50 +0100

    select for calendar

--- TogetherNotes/Models/Management/ContractsOrm.cs
+++ TogetherNotes/Models/Management/ContractsOrm.cs
@@ -1,6 +1,34 @@
-﻿namespace TogetherNotes.Models.Management
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Data.SqlClient;
+
+namespace TogetherNotes.Models.Management
 {
     public static class ContractsOrm
     {
+        public static List<object> GetEventsByDate(DateTime selectedDate)
+        {
+            try
+            {
+                return Orm.db.contracts
+                    .Where(e => e.init_hour.Date == selectedDate.Date)
+                    .Select(e => new
+                    {
+                        Time = e.init_hour.ToString("HH:mm"),
+                        Title = e.meet_type
+                    })
+                    .ToList<object>(); 
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(Orm.ErrorMessage(ex));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("General error: " + ex.Message);
+            }
+            return new List<object>();
+        }
     }
 }
--- TogetherNotes/ViewModel/CalendarVM.cs
+++ TogetherNotes/ViewModel/CalendarVM.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Windows.Controls;
 using System.Windows.Input;
+using TogetherNotes.Models.Management;
 using TogetherNotes.Utils;
 
 namespace TogetherNotes.ViewModel
@@ -11,6 +12,7 @@ namespace TogetherNotes.ViewModel
     public class CalendarVM : ViewModelBase
     {
         private DateTime? _selectedDate;
+
         public DateTime? SelectedDate
         {
             get => _selectedDate;
@@ -18,50 +20,29 @@ namespace TogetherNotes.ViewModel
             {
                 _selectedDate = value;
                 OnPropertyChanged();
-                FilterEvents();
+                LoadEventsFromDatabase();
             }
         }
 
-        public ObservableCollection<Event> Events { get; set; }
         public ObservableCollection<object> FilteredEvents { get; set; }
 
         public CalendarVM()
         {
-            Events = new ObservableCollection<Event>
-            {
-                new Event("2024-01-10 10:00:00.0000000 -05:00", "Post en redes sociales"),
-                new Event("2024-01-10 12:30:00.0000000 -05:00", "Revisar diseño de la app"),
-                new Event("2024-01-11 14:00:00.0000000 -05:00", "Construir demo de la app"),
-                new Event("2024-01-11 19:30:00.0000000 -05:00", "Cena con John Doe"),
-                new Event("2024-01-12 08:45:00.0000000 -05:00", "Enviar correo a Mohammad")
-            };
             FilteredEvents = new ObservableCollection<object>();
         }
 
-        private void FilterEvents()
+        private void LoadEventsFromDatabase()
         {
-            if (SelectedDate.HasValue)
-            {
-                var filtered = Events
-                    .Where(ev => ev.Timestamp.Date == SelectedDate.Value.Date)  
-                    .Select(ev => new
-                    {
-                        Time = ev.Timestamp.LocalDateTime.ToString("HH:mm"), 
-                        ev.Title
-                    })
-                    .ToList();
+            if (!SelectedDate.HasValue) return;
 
-                FilteredEvents.Clear();
-                foreach (var item in filtered)
-                {
-                    FilteredEvents.Add(item);
-                }
+            var events = ContractsOrm.GetEventsByDate(SelectedDate.Value);
+
+            FilteredEvents.Clear();
+            foreach (var item in events)
+            {
+                FilteredEvents.Add(item);
             }
         }
-
-        private DateTime ConvertTimestampToDate(long timestamp)
-        {
-            return DateTimeOffset.FromUnixTimeSeconds(timestamp).DateTime;
-        }
     }
+
 }
